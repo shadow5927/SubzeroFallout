e9fdbfc0a696636ebe78b5f4e119fe344531676a
Fractured_Shadow <68168890+shadow5927@users.noreply.github.com>
2026-04-05 11:26:29 -0400

    Content Port Pt.2?
    Sprites soon, testing to make sure it works

--- src/main/content/SubzeroFalloutBlocks.java
+++ src/main/content/SubzeroFalloutBlocks.java
@@ -3,12 +3,8 @@ package main.content;
 import arc.graphics.*;
 import arc.math.*;
 import arc.struct.*;
-import main.content.SubzeroFalloutItems;
-import main.content.SubzeroFalloutLiquids;
 import mindustry.*;
-import mindustry.content.Items;
-import mindustry.content.Liquids;
-import mindustry.content.UnitTypes;
+import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
@@ -72,7 +68,7 @@ public class SubzeroFalloutBlocks {
             requirements(Category.effect, with(Items.graphite, 500, Items.silicon, 800, Items.beryllium, 500));
             size = 3;
             isFirstTier = true;
-            unitType = UnitTypes.poly;
+            unitType = SubzeroFalloutUnits.enkindle;
             health = 2500;
             itemCapacity = 1000;
             armor = 5f;
@@ -108,41 +104,164 @@ public class SubzeroFalloutBlocks {
             heatRequirement = 5f;
             craftTime = 60f * 2f;
             liquidCapacity = 80f * 5;
+            consumeItem(Items.silicon, 3);
             outputItem = new ItemStack(SubzeroFalloutItems.iron, 1);
             ambientSound = Sounds.loopSmelter;
             ambientSoundVolume = 0.9f;
         }};
 
         bulkDriver = new MassDriver("bulk-driver"){{
-
+            requirements(Category.distribution, with(Items.graphite, 50));
+            size = 2;
+            itemCapacity = 100;
+            reload = 150f;
+            range = 300f;
+            consumePower(1.5f);
         }};
 
-        windmill = new MassDriver("windmill"){{
-
+        windmill = new SolarGenerator("windmill"){{
+            requirements(Category.power, with(Items.graphite, 50));
+            size = 2;
+            powerProduction = 1.5f / 6f;
         }};
 
         capacitor = new Battery("capacitor"){{
-
+            requirements(Category.power, with(Items.graphite, 50));
+            size = 2;
+            consumePowerBuffered(7500f);
+            consumePower(0.5f);
+            baseExplosiveness = 2.5f;
         }};
 
         thermalSiphon = new ThermalGenerator("thermal-siphon"){{
-
+            requirements(Category.power, with(Items.graphite, 50));
+            size = 3;
+            powerProduction = 2f;
+            floating = true;
+            ambientSound = Sounds.loopHum;
+            ambientSoundVolume = 0.04f;
         }};
 
         carbonHeater = new HeatProducer("carbon-heater"){{
-
+            requirements(Category.crafting, with(Items.graphite, 50));
+            size = 2;
+            liquidCapacity = 10f;
+            heatOutput = 2.5f;
+            craftTime = 60f * 9f;
+            ambientSound = Sounds.loopHum;
+            consumeLiquid(SubzeroFalloutLiquids.carbon, 4f / 60f);
         }};
 
         rapidAssembler = new UnitFactory("rapid-assembler"){{
-
+            requirements(Category.units, with(Items.graphite, 50));
+            plans = Seq.with(
+                    new UnitPlan(SubzeroFalloutUnits.flare, 60f * 40, with(Items.silicon, 40, SubzeroFalloutItems.iron, 50)),
+                    new UnitPlan(SubzeroFalloutUnits.delta, 60f * 20, with(Items.silicon, 50, Items.graphite, 30)),
+                    new UnitPlan(SubzeroFalloutUnits.sickle, 60f * 50, with(Items.silicon, 30, SubzeroFalloutItems.iron, 60, Items.graphite, 20))
+            );
+            consumePower(1.5f);
+            size = 4;
         }};
 
         supportAssembler = new UnitFactory("support-assembler"){{
-
+            requirements(Category.units, with(Items.graphite, 50));
+            plans = Seq.with(
+                    new UnitPlan(SubzeroFalloutUnits.nerve, 60f * 50, with(Items.silicon, 70, Items.graphite, 30))
+            );
+            consumePower(1.5f);
+            size = 4;
         }};
 
         specialistAssembler = new UnitFactory("specialist-assembler"){{
+            requirements(Category.units, with(Items.graphite, 50));
+            plans = Seq.with(
+            );
+            consumePower(1.5f);
+            size = 4;
+        }};
 
+        spear = new PowerTurret("spear"){{
+            requirements(Category.turret, with(Items.graphite, 50));
+            size = 3;
+            shootType = new LaserBulletType(140){{
+                colors = new Color[]{Pal.lancerLaser.cpy().a(0.4f), Pal.lancerLaser, Color.black};
+                chargeEffect = new MultiEffect(Fx.lancerLaserCharge, Fx.lancerLaserChargeBegin);
+
+                buildingDamageMultiplier = 0.25f;
+                hitEffect = Fx.hitLancer;
+                hitSize = 4;
+                lifetime = 16f;
+                drawSize = 400f;
+                pierceBuilding = true;
+                length = 173f;
+                ammoMultiplier = 1f;
+                pierceCap = 4;
+            }};
+            shootWarmupSpeed = 0.08f;
+            range = 165f;
+            scaledHealth = 420;
+            recoil = 3f;
+            reload = 80f;
+            shake = 2f;
+            shootEffect = Fx.lancerLaserShoot;
+            smokeEffect = Fx.none;
+            heatColor = Color.red;
+            chargeSound = Sounds.chargeLancer;
+            shootSound = Sounds.shootLancer;
+        }};
+
+        sc1 = new ItemTurret("sc-1"){{
+            requirements(Category.turret, with(Items.graphite, 50));
+            size = 3;
+            ammo(
+                Items.thorium, new ArtilleryBulletType(6f, 350, "shell"){{
+                hitEffect = new MultiEffect(Fx.titanExplosion, Fx.titanSmoke);
+                despawnEffect = Fx.none;
+                knockback = 2f;
+                lifetime = 140f;
+                height = 19f;
+                width = 17f;
+                splashDamageRadius = 20f;
+                splashDamage = 350f;
+                scaledSplashDamage = true;
+                backColor = hitColor = trailColor = Color.valueOf("fffd8b").lerp(Pal.redLight, 0.5f);
+                frontColor = Color.white;
+                ammoMultiplier = 1f;
+                hitSound = Sounds.explosionTitan;
+
+                status = StatusEffects.slow;
+                statusDuration = 100f;
+
+                trailLength = 32;
+                trailWidth = 3.35f;
+                trailSinScl = 2.5f;
+                trailSinMag = 0.5f;
+                trailEffect = Fx.none;
+                despawnShake = 7f;
+                shootEffect = Fx.shootTitan;
+                smokeEffect = Fx.shootSmokeTitan;
+                trailInterp = v -> Math.max(Mathf.slope(v), 0.8f);
+                shrinkX = 0.2f;
+                shrinkY = 0.1f;
+                buildingDamageMultiplier = 0.3f;
+            }});
+            shootSound = Sounds.shootTank;
+            ammoPerShot = 4;
+            maxAmmo = ammoPerShot * 3;
+            shake = 4f;
+            recoil = 1f;
+            reload = 60f * 2.3f;
+            shootY = 7f;
+            rotateSpeed = 1.4f;
+            minWarmup = 0.85f;
+            newTargetInterval = 40f;
+            warmupMaintainTime = 120f;
+            shootWarmupSpeed = 0.08f;
+            outlineColor = Pal.darkOutline;
+            consumeLiquid(Liquids.hydrogen, 5f / 60f);
+            scaledHealth = 250;
+            range = 390f;
+            size = 4;
         }};
     }
 
--- src/main/content/SubzeroFalloutUnits.java
+++ src/main/content/SubzeroFalloutUnits.java
@@ -1,4 +1,180 @@
 package main.content;
 
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.ai.types.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.type.unit.*;
+import mindustry.type.weapons.*;
+import mindustry.world.meta.*;
+
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+
 public class SubzeroFalloutUnits {
+    public static UnitType
+    //TYR
+    enkindle,
+    //Rapid
+    flare, delta, sickle,
+    //Support
+    nerve
+    //Specialist
+
+    //ALTECORIS
+
+    //LEGION
+
+    ;
+
+    public static void load(){
+    enkindle = new TankUnitType("enkindle"){{
+        coreUnitDock = true;
+        isEnemy = false;
+        envDisabled = 0;
+
+        range = 60f;
+        faceTarget = true;
+        targetPriority = -2;
+        lowAltitude = false;
+        mineWalls = true;
+        mineFloor = false;
+        mineHardnessScaling = false;
+        flying = true;
+        mineSpeed = 6f;
+        mineTier = 3;
+        buildSpeed = 1.2f;
+        drag = 0.08f;
+        speed = 5.6f;
+        rotateSpeed = 7f;
+        accel = 0.09f;
+        itemCapacity = 60;
+        health = 300f;
+        armor = 1f;
+        hitSize = 9f;
+        engineSize = 0;
+        payloadCapacity = 2f * 2f * tilesize * tilesize;
+        pickupUnits = false;
+        vulnerableWithPayloads = true;
+
+        fogRadius = 0f;
+        targetable = false;
+        hittable = false;
+
+        setEnginesMirror(
+                new UnitEngine(21 / 4f, 19 / 4f, 2.2f, 45f),
+                new UnitEngine(23 / 4f, -22 / 4f, 2.2f, 315f)
+        );
+
+        weapons.add(new RepairBeamWeapon(){{
+            widthSinMag = 0.11f;
+            reload = 20f;
+            x = 0f;
+            y = 6.5f;
+            rotate = false;
+            shootY = 0f;
+            beamWidth = 0.7f;
+            repairSpeed = 3.1f;
+            fractionRepairSpeed = 0.06f;
+            aimDst = 0f;
+            shootCone = 15f;
+            mirror = false;
+
+            targetUnits = false;
+            targetBuildings = true;
+            autoTarget = false;
+            controllable = true;
+            laserColor = Pal.accent;
+            healColor = Pal.accent;
+
+            bullet = new BulletType(){{
+                maxRange = 60f;
+            }};
+        }});
+    }};
+
+    flare = new TankUnitType("flare"){{
+        hitSize = 12f;
+        treadPullOffset = 3;
+        speed = 0.75f;
+        rotateSpeed = 3.5f;
+        health = 850;
+        armor = 6f;
+        itemCapacity = 0;
+        floorMultiplier = 0.95f;
+        treadRects = new Rect[]{new Rect(12 - 32f, 7 - 32f, 14, 51)};
+        researchCostMultiplier = 0f;
+
+        tankMoveVolume *= 0.32f;
+        tankMoveSound = Sounds.tankMoveSmall;
+    }};
+
+    delta = new UnitType("delta"){{
+        canBoost = true;
+        boostMultiplier = 1.5f;
+        speed = 0.55f;
+        hitSize = 8f;
+        health = 120f;
+        armor = 1f;
+
+    }};
+
+    sickle = new UnitType("sickle"){{
+        naval = true;
+        speed = 1.1f;
+        drag = 0.13f;
+        hitSize = 10f;
+        health = 280;
+        armor = 2f;
+        accel = 0.4f;
+        rotateSpeed = 3.3f;
+        faceTarget = false;
+
+        trailLength = 20;
+        waveTrailX = 4f;
+        trailScl = 1.3f;
+
+        moveSoundVolume = 0.4f;
+        moveSound = Sounds.shipMove;
+    }};
+
+    nerve = new UnitType("nerve"){{
+        flying = true;
+        speed = 2.7f;
+        accel = 0.08f;
+        drag = 0.04f;
+        health = 70;
+        engineOffset = 5.75f;
+        targetFlags = new BlockFlag[]{BlockFlag.generator, null};
+        hitSize = 9;
+        itemCapacity = 10;
+        targetPriority = -2;
+        circleTarget = false;
+        omniMovement = false;
+        rotateSpeed = 5f;
+        circleTargetRadius = 60f;
+        wreckSoundVolume = 0.7f;
+
+        moveSound = Sounds.loopThruster;
+        moveSoundPitchMin = 0.6f;
+        moveSoundVolume = 0.2f;
+    }};
+    }
 }
+
